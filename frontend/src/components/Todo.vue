756205dde59f6f8d9642e425bfe3a86ec18a3476
a-mt <amistri@live.fr>
2025-01-17 15:40:36 +0100

    Add createAt

--- frontend/src/components/Todo.vue
+++ frontend/src/components/Todo.vue
@@ -40,15 +40,19 @@
           class="ml-2"
         >
           {{ todo.title }}
+          <small class="text-xs text-gray-500 block">
+            {{ new Date(todo.createdAt).toLocaleDateString() }}
+          </small>
         </span>
       </div>
     </div>
 
     <!-- Action buttons -->
     <div class="flex items-center">
+
       <!-- Set priority -->
       <button
-        class="w-5 h-5 text-gray-500 hover:text-gray-700 bg-gray-200 rounded text-sm text-center mr-2"
+        class="mr-3 w-5 h-5 text-gray-500 hover:text-gray-700 bg-gray-200 rounded text-sm text-center mr-2"
         @click="showPriorityDialog = true"
       >
         {{ todo.priority }}
@@ -163,7 +167,6 @@
 
 <script>
 import axios from 'axios';
-import Sortable from 'sortablejs';
 import TagSelect from './TagSelect.vue';
 import NotificationMixin from '../mixins/NotificationMixin.js';
 
